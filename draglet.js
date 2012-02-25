cc783521cbe8d42996565f8590891b93426bed0a
John Allison <jrallison@gmail.com>
2012-02-25 17:12:39 -0500

    Move css out to it's own file

--- draglet.js
+++ draglet.js
@@ -1,41 +1,48 @@
-(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||f.fn.jquery||h(f)){c=a.createElement('script');c.type='text/javascript';c.src='http://ajax.googleapis.com/ajax/libs/jquery/'+g+'/jquery.min.js';c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=='loaded'||d=='complete')){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,'1.7.1',function($,L){
+(function() {
 
+  function loadDraglet($, loaded) { 
+    insertDraglet($);
+    setupEvents($);
+  }
 
-  var styles='#dragletOverlay { position: fixed; z-index: 9999; top: 0; right: 0; bottom: 0; left: 0; background-color: black; opacity: .6; } #dragletContainer { font-family: Helvetica, arial, sans-serif; position: fixed; z-index: 100000002; top: 50%; left: 50%; margin: -150px 0 0 -300px; padding: 20px; width: 560px; height: 260px; background-color: #222; border-radius: 3px; opacity: 1; } #dragletContainer h1 { margin: 0 !important; padding: 0 !important; color: #333 !important; font-size: 40px !important; line-height: 260px !important; font-family: HelveticaNeue, \'Helvetica Neue\', Helvetica, Arial, sans-serif !important; text-transform: uppercase !important; text-align: center !important;';
+  function insertDraglet($) {
+    $('body').append('<div id=\'dragletOverlay\'></div>');
+    $('body').append('<div id=\'dragletContainer\'><h1>Drop image here</h1></div>');
+  }
 
-  var stylesheet = document.createElement('link');
-  stylesheet.rel = 'stylesheet';
-  stylesheet.href = 'data:text/css,' + escape(styles);
+  function setupEvents($) {
+    // Removal
+    $('#dragletOverlay').click(removeDraglet);
 
-  $('body').append(stylesheet);
-  $('body').append('<div id=\'dragletOverlay\'></div>');
-  $('body').append('<div id=\'dragletContainer\'><h1>Drop image here</h1></div>');
+    $(window).on('keyup', function(e) {
+      if (e.keyCode == 27) { removeDraglet(); }
+    });
 
-  $('#dragletOverlay').click(function() {
-    $('#dragletOverlay,#dragletContainer').remove();
-  });
+    // Drag'n drop
+    $('#dragletContainer').on({
+      dragover: function(e) {
+        e.preventDefault();
+      },
+      drop: function(e) {
+        console.log(e);
+        e.preventDefault();
+        upload($, e.originalEvent.dataTransfer.files[0]);
+      }
+    });
 
-  $(window).on('keyup', function(e) {
-    if (e.keyCode == 27) { $('#dragletOverlay,#dragletContainer').remove(); }
-  });
+    // Select all on focus
+    $("#dragletContainer").on("click", "input", function() {
+      this.select();
+    });
 
-  /* Drag'n drop stuff */
-  $('#dragletContainer').on({
-    dragover: function(e) {
-      e.preventDefault();
-    },
-    drop: function(e) {
-      e.preventDefault();
-      upload(e.originalEvent.dataTransfer.files[0]);
+    function removeDraglet() {
+      $('#dragletOverlay,#dragletContainer').remove();
     }
-  });
+  }
 
-  function upload(file) {
+  function upload($, file) {
     /* Is the file an image? */
     if (!file || !file.type.match(/image.*/)) return;
-
-    /* It is! */
-    // TODO SHOW LOADING
 
     var data = new FormData();
     data.append('image', file);
@@ -44,11 +51,34 @@
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://api.imgur.com/2/upload.json');
     xhr.onload = function() {
-      var url = JSON.parse(xhr.responseText).upload.links.original; 
-      $('#dragletContainer').html('<input type=\'text\' value=\'' + url + '\' />');
-      $('#dragletContainer').append('<input type=\'text\' value=\'![my image](' + url + ')\' />');
+      showDragletUrl($, JSON.parse(xhr.responseText).upload.links.original);
     };
 
     xhr.send(data);
   }
-});
+
+  function showDragletUrl($, url) {
+    $('#dragletContainer').html('<label for=\'dragletUrl\'>URL</label><input id=\'dragletUrl\' type=\'text\' value=\'' + url + '\' />');
+    $('#dragletContainer').append('<label for=\'dragletMarkdown\'>Markdown</label><input id=\'dragletMarkdown\' type=\'text\' value=\'![my image](' + url + ')\' />');
+  }
+
+  // Load jquery 1.7.1 if it isn't already loaded
+  function loadJQuery(window, document, req_version, callback, $, script, done, readystate) {
+    if (!($ = window.jQuery) || req_version > $.fn.jquery || callback($)) {
+      script  = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src  = 'http://ajax.googleapis.com/ajax/libs/jquery/' + req_version + '/jquery.min.js';
+      script.onload = script.onreadystatechange = function() {
+        if(!done && (!(readystate = this.readyState) || readystate == 'loaded' || readystate == 'complete')) {
+          $ = window.jQuery;
+          callback($.noConflict(1), done = 1);
+          $(script).remove();
+        }
+      };
+      document.documentElement.childNodes[0].appendChild(script);
+    }
+  }
+  
+  loadJQuery(window, document, '1.7.1', loadDraglet);
+})();
+

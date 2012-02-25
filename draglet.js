8cce5db959fae7eba2471aaf41942bd47709c7ec
John Allison <jrallison@gmail.com>
2012-02-25 00:59:37 -0500

    Add imgur links to popup after generation

--- draglet.js
+++ draglet.js
@@ -1,6 +1,7 @@
 (function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||f.fn.jquery||h(f)){c=a.createElement('script');c.type='text/javascript';c.src='http://ajax.googleapis.com/ajax/libs/jquery/'+g+'/jquery.min.js';c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=='loaded'||d=='complete')){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,'1.7.1',function($,L){
 
-  var styles='#dragletOverlay { position: fixed; z-index: 9999; top: 0; right: 0; bottom: 0; left: 0; background-color: black; opacity: .6; } #dragletContainer { font-family: Helvetica, arial, sans-serif; position: fixed; z-index: 100000002; top: 50%; left: 50%; margin: -150px 0 0 -300px; width: 600px; height: 300px; background-color: #222; border-radius: 3px; opacity: 1; }';
+
+  var styles='#dragletOverlay { position: fixed; z-index: 9999; top: 0; right: 0; bottom: 0; left: 0; background-color: black; opacity: .6; } #dragletContainer { font-family: Helvetica, arial, sans-serif; position: fixed; z-index: 100000002; top: 50%; left: 50%; margin: -150px 0 0 -300px; padding: 20px; width: 560px; height: 260px; background-color: #222; border-radius: 3px; opacity: 1; } #dragletContainer h1 { margin: 0 !important; padding: 0 !important; color: #333 !important; font-size: 40px !important; line-height: 260px !important; font-family: HelveticaNeue, \'Helvetica Neue\', Helvetica, Arial, sans-serif !important; text-transform: uppercase !important; text-align: center !important;';
 
   var stylesheet = document.createElement('link');
   stylesheet.rel = 'stylesheet';
@@ -8,7 +9,7 @@
 
   $('body').append(stylesheet);
   $('body').append('<div id=\'dragletOverlay\'></div>');
-  $('body').append('<div id=\'dragletContainer\'></div>');
+  $('body').append('<div id=\'dragletContainer\'><h1>Drop image here</h1></div>');
 
   $('#dragletOverlay').click(function() {
     $('#dragletOverlay,#dragletContainer').remove();
@@ -43,8 +44,9 @@
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://api.imgur.com/2/upload.json');
     xhr.onload = function() {
-      alert(JSON.parse(xhr.responseText).upload.links.original); 
-      // TODO DO SOMETHING WITH IT
+      var url = JSON.parse(xhr.responseText).upload.links.original; 
+      $('#dragletContainer').html('<input type=\'text\' value=\'' + url + '\' />');
+      $('#dragletContainer').append('<input type=\'text\' value=\'![my image](' + url + ')\' />');
     };
 
     xhr.send(data);

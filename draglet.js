fb3d0555273aa46f64df1697c200239b027ba05d
John Allison <jrallison@gmail.com>
2012-02-27 16:27:31 -0500

    load jquery with whatever protocol

--- draglet.js
+++ draglet.js
@@ -67,7 +67,7 @@
     if (!($ = window.jQuery) || req_version > $.fn.jquery || callback($)) {
       script  = document.createElement('script');
       script.type = 'text/javascript';
-      script.src  = 'http://ajax.googleapis.com/ajax/libs/jquery/' + req_version + '/jquery.min.js';
+      script.src  = '//ajax.googleapis.com/ajax/libs/jquery/' + req_version + '/jquery.min.js';
       script.onload = script.onreadystatechange = function() {
         if(!done && (!(readystate = this.readyState) || readystate == 'loaded' || readystate == 'complete')) {
           $ = window.jQuery;
